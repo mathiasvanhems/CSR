cf7ca1cbf12172badf633ba9690a4cb267f890c9
Mathias Vanhems <vanhems.mathias@gmail.com>
2023-04-03 00:04:13 +0200

    Mise en forme linux

--- generate.sh
+++ generate.sh
@@ -1,12 +1,10 @@
 cd out/
 
-:: Creation d'une clef privée
+# Creation d'une clef privée
 openssl genrsa -out private.key 4096
 
-:: Creation c'un CSR (Certificat Server Request)
+# Creation c'un CSR (Certificat Server Request)
 openssl req -new -key private.key -out domain.csr -config ../detail.conf
 
-:: Creation d'un certificat autosigné
+# Creation d'un certificat autosigné
 openssl x509 -req -in domain.csr -key private.key -out domain.crt -days 365 -sha256
-
-pause
